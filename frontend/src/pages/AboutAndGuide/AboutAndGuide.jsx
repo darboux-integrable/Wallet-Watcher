fe79966ce14916461264b082221ccbc977e7e9e5
darboux-integrable <adamevanswork1@gmail.com>
2025-02-22 21:48:54 -0500

    Tylers work
    here it is sorry it took so long, I was having a midlife crisis

--- frontend/src/pages/AboutAndGuide/AboutAndGuide.jsx
+++ frontend/src/pages/AboutAndGuide/AboutAndGuide.jsx
@@ -2,20 +2,37 @@ import styles from "./AboutAndGuide.module.css"
 import Navbar from "../../components/Navbar/Navbar"
 export default function AboutAndGuide(props) {
     return (
-        <div>
-            <Navbar position="relative"></Navbar>
+        <div className={styles.pageContent}>
+            <Navbar position="fixed"></Navbar>
 
-
-            <Show when={props.url == "about"}>
-                
-            </Show>
-            <Show when={props.url == "guide"}>
-
-            </Show>
             
+            <div className={styles.page}>
+                <div className={styles.pageBox}>
+                    <div className={styles.sideNav}>
+                        <button>
+                            About
+                        </button>
+                        <button>
+                            Guide
+                        </button>
+                    </div>
 
+                    <div className={styles.contentWrapper}>
+                        <Show when={props.url == "about"}>
+                            <div>
+                                content2
+                            </div>
+                        </Show>
+                        <Show when={props.url == "guide"}>
+                            <div>
+                                content
+                            </div>
+                        </Show>
 
-
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
