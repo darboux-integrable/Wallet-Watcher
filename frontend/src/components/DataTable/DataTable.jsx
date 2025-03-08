36fd4498e20440be3b0fc8fc3290e305bde2139d
darboux-integrable <adamevanswork1@gmail.com>
2025-03-08 15:50:23 -0500

    more work on the landing page

---
+++ frontend/src/components/DataTable/DataTable.jsx
@@ -0,0 +1,28 @@
+import styles from "./DataTable.module.css"
+
+export default function DataTable({ title, data}){
+
+    return (
+
+        <div className={styles.table}>
+            <h1 className={styles.title}>{title}</h1>
+            <div className={styles.dataSection}>
+                {data.map(dataPoint => {
+                    return (
+                        <div className={styles.dataPoint}>
+                            <div className={styles.topSection}>
+                                <h2 className={styles.dataTitle}>{dataPoint.title}</h2>
+                            </div>
+                            <div className={styles.bottomSection}>
+                                <h2 className={styles.dataValue}>${dataPoint.value}</h2>
+                                <p className={styles.timeInterval}>{dataPoint.timeInterval}</p>
+                                </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+      
+    )
+
+}
--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,145 +1,179 @@
 import styles from "./LandingPage.module.css";
 import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer"
+import Footer from "../../components/Footer/Footer";
 import LandingChart from "./LandingChart";
 import LandingNav from "../../components/LandingNav/LandingNav";
+import DataTable from "../../components/DataTable/DataTable";
+import upIcon from "../../assets/images/up.svg";
 
 export default function LandingPage() {
+  let data = [
+    {
+      title: "Work",
+      timeInterval: "Monthly",
+      value: 1200,
+    },
+    {
+      title: "Work",
+      timeInterval: "Monthly",
+      value: 1200,
+    },
+    {
+      title: "Work",
+      timeInterval: "Monthly",
+      value: 1200,
+    },
+    {
+      title: "Work",
+      timeInterval: "Monthly",
+      value: 1200,
+    },
+    {
+      title: "Work",
+      timeInterval: "Monthly",
+      value: 1200,
+    },
+    {
+      title: "Work",
+      timeInterval: "Monthly",
+      value: 1200,
+    },
+  ];
+
   return (
-  <div className={styles.landingPage}>
-    <Navbar position="fixed"></Navbar>
+    <div className={styles.landingPage}>
+      <Navbar position="fixed"></Navbar>
 
-    {/** Page Content */}
-    {/** Section 1 */}
-    <div className={styles.landingSection1}>
-      <div className={styles.landingSection1Left}>
-        <h1>The Financial</h1>
-        <h1>Tracker</h1>
-        <h2>For all your financial needs</h2>
-        <button>Get Started</button>
-      </div>
-
-      <div className={styles.landingSection1Right}>
-        <div className={styles.graphSquircle}>
-          <LandingChart></LandingChart>
+      {/** Page Content */}
+      {/** Section 1 */}
+      <div className={styles.landingSection1}>
+        <div className={styles.landingSection1Left}>
+          <h1>The Financial</h1>
+          <h1>Tracker</h1>
+          <h2>For all your financial needs</h2>
+          <button>Get Started</button>
         </div>
-      </div>
-    </div>
 
-    {/** Landing Nav */}
-    <div className={styles.landingNavWrapper}>
-      <LandingNav></LandingNav>
-    </div>
-
-    {/** Section 2 */}
-    <div classList={{ 
-      [styles.landingSection2]: true, 
-      [styles.landingSection]: true 
-    }}>
-
-      <div className={styles.sectionLeft}>
-        Hello
-      </div>
-
-      <div className={styles.sectionRight}>
-        <div className={styles.whiteBox}>
-          <h1>
-            Content:
-          </h1>
-          <p>
-            Hello
-          </p>
+        <div className={styles.landingSection1Right}>
+          <div className={styles.graphSquircle}>
+            <LandingChart></LandingChart>
+          </div>
         </div>
       </div>
 
+      {/** Landing Nav */}
+      <div className={styles.landingNavWrapper}>
+        <LandingNav></LandingNav>
+      </div>
 
-    </div>
-
-    {/** Section 3 */}
-    <div classList={{ 
-      [styles.landingSection3]: true, 
-      [styles.landingSection]: true 
-    }}>
-      <div className={styles.sectionLeft}>
-        <div className={styles.whiteBox}>
-            <h1>
-              Content:
-            </h1>
-            <p>
-              Hello
-            </p>
+      {/** Section 2 */}
+      <div
+        classList={{
+          [styles.landingSection2]: true,
+          [styles.landingSection]: true,
+        }}
+      >
+        <div className={styles.sectionLeft}>
+          <div className={styles.invertedTitle}>Incomes</div>
+          <div className={styles.incomesText}>
+            You can enter different income streams. Anything counts, your job, a
+            side gig, money from a birthday card from gandma, anything! Wallet
+            Watcher will keep track of these different incomes.
+            <br />
+            <br />
+            Incomes can see viewed in tables as shown on the right, but also as
+            graphical data which may better help you visualize your incomes over
+            time!
           </div>
         </div>
 
-      <div className={styles.sectionRight}>
-        Hello
+        <div className={styles.sectionRight}>
+          <DataTable data={data} title={"Incomes"} />
+        </div>
       </div>
-    </div>
 
-    {/** Section 4 */}
-    <div classList={{ 
-      [styles.landingSection4]: true, 
-      [styles.landingSection]: true 
-    }}>
+      {/** Section 3 */}
+      <div
+        classList={{
+          [styles.landingSection3]: true,
+          [styles.landingSection]: true,
+        }}
+      >
+        <div className={styles.sectionLeft}>
+          <DataTable data={data} title={"Incomes"} />
+        </div>
 
-      <div className={styles.sectionLeft}>
-        <div className={styles.section4DroppingPanels}>
-          <div className={styles.dropPanel}>
-            <div>
-              <li>Hello</li>
+        <div className={styles.sectionRight}>
+          <div className={styles.invertedTitle}>Incomes</div>
+          <div className={styles.incomesText}>
+            Just like incomes, your expenses can also be viewed in a table or
+            graph to visualize the data. Expenses can be anything from your
+            rent, car payments, student loans, food, electricity, or a $20 bet
+            you lost. 
+
+            <br />
+            <br />
+
+            Your expenses along with your incomes can come together to calculuate your net (how much you profited or lost in total). 
+          </div>
+        </div>
+      </div>
+
+      {/** Section 4 */}
+      <div
+        classList={{
+          [styles.landingSection4]: true,
+          [styles.landingSection]: true,
+        }}
+      >
+        <div className={styles.sectionLeft}>
+          <div className={styles.section4DroppingPanels}>
+            <div className={styles.dropPanel}>
+              <div>
+                <li>Hello</li>
+              </div>
+            </div>
+            <div className={styles.dropPanel}>
+              <div>
+                <li>Hello</li>
+              </div>
+            </div>
+            <div className={styles.dropPanel}>
+              <div>
+                <li>Hello</li>
+              </div>
             </div>
           </div>
-          <div className={styles.dropPanel}>
-            <div>
-              <li>Hello</li>
-            </div>
-          </div>
-          <div className={styles.dropPanel}>
-            <div>
-              <li>Hello</li>
-            </div>
-          </div>
         </div>
 
-      </div>
-
-      <div className={styles.sectionRight}>
-        <div className={styles.whiteBox}>
-          <h1>
-            Content:
-          </h1>
-          <p>
-            Hello
-          </p>
+        <div className={styles.sectionRight}>
+          <div className={styles.whiteBox}>
+            <h1>Content:</h1>
+            <p>Hello</p>
+          </div>
         </div>
       </div>
 
-    </div>
-
-    {/** Section 5 */}
-    <div classList={{ 
-      [styles.landingSection5]: true, 
-      [styles.landingSection]: true 
-    }}>      
-      <div className={styles.sectionLeft}>
-        <div className={styles.whiteBox}>
-            <h1>
-              Content:
-            </h1>
-            <p>
-              Hello
-            </p>
+      {/** Section 5 */}
+      <div
+        classList={{
+          [styles.landingSection5]: true,
+          [styles.landingSection]: true,
+        }}
+      >
+        <div className={styles.sectionLeft}>
+          <div className={styles.whiteBox}>
+            <h1>Content:</h1>
+            <p>Hello</p>
           </div>
         </div>
 
-      <div className={styles.sectionRight}>
-        Hello
+        <div className={styles.sectionRight}>Hello</div>
+      </div>
+
+      <div className={styles.footerWrapper}>
+        <Footer></Footer>
       </div>
     </div>
-
-    <div className={styles.footerWrapper}>
-      <Footer></Footer>
-    </div>
-  </div>
   );
 }
